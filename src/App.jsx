973d1fd27a278f5b4dc9609e0c97fc841a3d1c95
Osvaldo Arzate Santiago <osvarz.new@gmail.com>
2022-05-26 22:29:03 -0500

    🎉 initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+	return (
+		<div>
+			<h1 className="text-medium">
+				Las mejores raquetas para
+				<br /> jugar <span className="bold">Tennis</span>
+				<img className="svg-white" src="./src/assets/img/play.svg" alt="" />
+			</h1>
+		</div>
+	)
+}
+
+export default App
